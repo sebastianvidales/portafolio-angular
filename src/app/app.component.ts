28213097ca6abdb28fd79e20e1c05347605cdbb8
Sebastian Vidales <s.vidales@udla.edu.co>
2020-02-05 11:34:37 -0500

    Fin de la sección 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { InfoPaginaService } from './services/info-pagina.service';
+import { ProductosService } from './services/productos.service';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { InfoPaginaService } from './services/info-pagina.service';
 })
 export class AppComponent {
 
-  constructor(public infopaginaservice: InfoPaginaService){
-      
+  constructor(public infopaginaservice: InfoPaginaService,
+              public productosService: ProductosService) {  
   }
 }
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+    cargando = true;
+    productos: Producto [] = [];
+  constructor( private http: HttpClient) { 
+    this.CargarProductos();
+  }
+
+  private CargarProductos(){
+    this.http.get('https://angular-html-d5090.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[] ) => {
+        console.log(resp);
+        this.productos = resp;
+        setTimeout(() => {
+          this.cargando = false;
+        }, 2000);
+      });
+  }
+}
