2c68111282dd1de8bb5cac43b71b9f7fe2ee2ae3
Sebastian Vidales <s.vidales@udla.edu.co>
2020-02-06 15:22:49 -0500

    Fin de sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,18 +9,64 @@ export class ProductosService {
 
     cargando = true;
     productos: Producto [] = [];
-  constructor( private http: HttpClient) { 
+    productosFiltrados: Producto [] = [];
+
+  constructor( private http: HttpClient) {
     this.CargarProductos();
   }
 
   private CargarProductos(){
-    this.http.get('https://angular-html-d5090.firebaseio.com/productos_idx.json')
+
+    return new Promise( (resolve, reject) => {
+      this.http.get('https://angular-html-d5090.firebaseio.com/productos_idx.json')
       .subscribe( (resp: Producto[] ) => {
-        console.log(resp);
         this.productos = resp;
         setTimeout(() => {
           this.cargando = false;
+          resolve();
         }, 2000);
       });
+    });
   }
+  getProducto(id: string){
+        return this.http.get(`https://angular-html-d5090.firebaseio.com/productos/${id}.json`);
+  }
+  buscarProducto(termino: string){
+    if(this.productos.length === 0){
+    // tslint:disable-next-line: comment-format
+    //cargar productos
+    this.CargarProductos().then( () => {
+      // tslint:disable-next-line: comment-format
+      //ejecutar despues de tener los filtros
+      // tslint:disable-next-line: comment-format
+      //aplicar filtro
+      this.filtrarProductos( termino );
+    });
+    } else {
+      // tslint:disable-next-line: comment-format
+      //aplicar el filtro
+      this.filtrarProductos( termino );
+
+    }
+  }
+  private filtrarProductos( termino: string){
+    console.log(this.productos);
+    /*lo unico que hacemos es cargar los datos que están en el arreglo
+    producto, al arreglo productosFiltrados, pero con la diferencia de que solo tendremos 
+    los datos buscados*/
+    this.productosFiltrados = [];
+
+    termino = termino.toLocaleLowerCase();  /*A la hora de ingresar la busqueda, se puede encontrar el 
+    articulo sin importar las mayusculas o minusculas*/
+
+    this.productos.forEach(prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase(); /* se ingresa en una variable 
+      constante y se aplica en la condición. Si la CATEGORIA tuviera minusculas y mayusculas, se aplica el mismo LocaleLowerCase */
+      if (prod.categoria.indexOf ( termino ) >= 0 || tituloLower.indexOf ( termino ) >= 0) {
+        this.productosFiltrados.push(prod);
+      }
+    });
+    }
 }
+
